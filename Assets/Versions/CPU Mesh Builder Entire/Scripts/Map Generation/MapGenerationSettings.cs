e0980efd99907a3c73996cbed70006a301fad00c
NumbersOneThroughJ <152826581+NumbersOneThroughJ@users.noreply.github.com>
2024-04-10 09:45:49 -0400

    Last minute Update
    Changed some heightmap terrain Generation. Will fork Heightmap generation into a separate project.

--- Assets/Versions/CPU Mesh Builder Entire/Scripts/Map Generation/MapGenerationSettings.cs
+++ Assets/Versions/CPU Mesh Builder Entire/Scripts/Map Generation/MapGenerationSettings.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 [SerializeField]
 public class MapGenerationSettings : MonoBehaviour
 {
+    [Range(0,5)]
     public float strength = 1;
+    [Range(0, 5)]
     public float baseRoughness = 1;
+    [Range(0, 5)]
     public float roughness = 2;
+    [Range(0, 5)]
     public float persistance = .5f;
+    [Range(0, 5)]
     public int numLayers = 3;
+    [Range(0, 1)]
+    public float valleyFactor = 0;
 }
